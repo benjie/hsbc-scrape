641260b3b5a715ab7a681e1d74076351c3319669
Benjie Gillam <benjie@jemjie.com>
2013-10-14 21:27:04 +0100

    Tidy transaction descriptions.

--- extradetails.js
+++ extradetails.js
@@ -1,4 +1,4 @@
 return {
-  description: $.trim($(".extContentHighlightPib table td:last p").text()),
+  description: $.trim($(".extContentHighlightPib table td:last p").text()).replace(/\n\s+/g, "\n"),
   back: $(".hsbcButtonBack")[0].href
 };
